c9378c3f0d87a4d53f6280d8f71cc6c43cfaf077
MorenaDlamini <dlaminimorena@gmail.com>
2025-01-26 21:13:45 +0200

    Check Empty input

--- scripts.js
+++ scripts.js
@@ -5,5 +5,10 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
-});
+
+  try {
+    // Check if inputs are empty
+    if (!dividend || !divider) {
+      result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+      return;
+    }
